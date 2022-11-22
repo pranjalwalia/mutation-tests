012083bf69dad55db967f3711ad4081f28b53648
Pranjal Walia <pranjalwalia77@gmail.com>
2022-11-22 17:00:46 +0530

    refactor: remotve type defs

--- src/linkedListNode.d.ts
+++
@@ -1,8 +0,0 @@
-export class LinkedListNode<T> {
-  constructor(value?: T, next?: LinkedListNode<T>);
-  setValue(value: T): LinkedListNode<T>;
-  getValue(): T;
-  setNext(next: LinkedListNode<T>): LinkedListNode<T>;
-  getNext(): LinkedListNode<T>;
-  hasNext(): boolean;
-}
