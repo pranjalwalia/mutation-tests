be392ca3bb3fc0dea5785041645a87c5224c58be
Pranjal Walia <pranjalwalia77@gmail.com>
2022-11-27 04:03:05 +0530

    feat: add queue

--- src/queue.js
+++ src/queue.js
@@ -5,136 +5,184 @@
  * @class
  */
 class Queue {
-  /**
-   * Creates a queue.
-   * @param {array} [elements]
-   */
-  constructor(elements) {
-    this._elements = Array.isArray(elements) ? elements : [];
-    this._offset = 0;
-  }
+    /**
+     * Creates a queue.
+     * @param {array} [elements]
+     */
+    constructor(elements) {
+        this._elements = Array.isArray(elements) ? elements : [];
+        this._offset = 0;
+    }
 
-  /**
-   * Adds an element to the back of the queue.
-   * @public
-   * @param {number|string|object} element
-   */
-  enqueue(element) {
-    this._elements.push(element);
-    return this;
-  }
+    /**
+     * Adds an element to the back of the queue.
+     * @public
+     * @param {number|string|object} element
+     */
+    enqueue(element) {
+        this._elements.push(element);
+        return this;
+    }
 
-  /**
-   * Adds an element to the back of the queue.
-   * @public
-   * @param {number|string|object} element
-   */
-  push(element) {
-    return this.enqueue(element);
-  }
+    reverse() {
+        let stack = [];
+        while (this._elements.length > 0) {
+            stack.push(this._elements[0]);
+            this._elements.shift();
+        }
+        while (stack.length > 0) {
+            this._elements.push(stack[stack.length - 1]);
+            stack.pop();
+        }
+    }
 
-  /**
-   * Dequeues the front element in the queue.
-   * @public
-   * @returns {number|string|object}
-   */
-  dequeue() {
-    if (this.size() === 0) return null;
+    /**
+     * Adds an element to the back of the queue.
+     * @public
+     * @param {number|string|object} element
+     */
+    push(element) {
+        return this.enqueue(element);
+    }
 
-    const first = this.front();
-    this._offset += 1;
+    sort() {
+        this._elements = this._elements.sort();
+    }
 
-    if (this._offset * 2 < this._elements.length) return first;
+    /**
+     * Dequeues the front element in the queue.
+     * @public
+     * @returns {number|string|object}
+     */
+    dequeue() {
+        if (this.size() === 0) return null;
 
-    // only remove dequeued elements when reaching half size
-    // to decrease latency of shifting elements.
-    this._elements = this._elements.slice(this._offset);
-    this._offset = 0;
-    return first;
-  }
+        const first = this.front();
+        this._offset += 1;
 
-  /**
-   * Dequeues the front element in the queue.
-   * @public
-   * @returns {number|string|object}
-   */
-  pop() {
-    return this.dequeue();
-  }
+        if (this._offset * 2 < this._elements.length) return first;
 
-  /**
-   * Returns the front element of the queue.
-   * @public
-   * @returns {number|string|object}
-   */
-  front() {
-    return this.size() > 0 ? this._elements[this._offset] : null;
-  }
+        // only remove dequeued elements when reaching half size
+        // to decrease latency of shifting elements.
+        this._elements = this._elements.slice(this._offset);
+        this._offset = 0;
+        return first;
+    }
 
-  /**
-   * Returns the back element of the queue.
-   * @public
-   * @returns {number|string|object}
-   */
-  back() {
-    return this.size() > 0 ? this._elements[this._elements.length - 1] : null;
-  }
+    /**
+     * Dequeues the front element in the queue.
+     * @public
+     * @returns {number|string|object}
+     */
+    pop() {
+        return this.dequeue();
+    }
 
-  /**
-   * Returns the number of elements in the queue.
-   * @public
-   * @returns {number}
-   */
-  size() {
-    return this._elements.length - this._offset;
-  }
+    /**
+     * Returns the front element of the queue.
+     * @public
+     * @returns {number|string|object}
+     */
+    front() {
+        return this.size() > 0 ? this._elements[this._offset] : null;
+    }
 
-  /**
-   * Checks if the queue is empty.
-   * @public
-   * @returns {boolean}
-   */
-  isEmpty() {
-    return this.size() === 0;
-  }
+    reverseFirstK(k) {
+        if (this._elements.length == 0 || k > this._elements.length)
+            return false;
+        if (k <= 0) return false;
+        let Stack = [];
+        for (let i = 0; i < k; i++) {
+            Stack.push(this._elements.shift());
+        }
+        while (Stack.length > 0) {
+            this._elements.push(Stack.pop());
+        }
+        for (let i = 0; i < this._elements.length - k; i++) {
+            this._elements.push(this._elements.shift());
+        }
+        return true;
+    }
 
-  /**
-   * Returns the remaining elements in the queue as an array.
-   * @public
-   * @returns {array}
-   */
-  toArray() {
-    return this._elements.slice(this._offset);
-  }
+    /**
+     * Returns the back element of the queue.
+     * @public
+     * @returns {number|string|object}
+     */
+    back() {
+        return this.size() > 0
+            ? this._elements[this._elements.length - 1]
+            : null;
+    }
 
-  /**
-   * Clears the queue.
-   * @public
-   */
-  clear() {
-    this._elements = [];
-    this._offset = 0;
-  }
+    /**
+     * Returns the number of elements in the queue.
+     * @public
+     * @returns {number}
+     */
+    size() {
+        return this._elements.length - this._offset;
+    }
 
-  /**
-   * Creates a shallow copy of the queue.
-   * @public
-   * @return {Queue}
-   */
-  clone() {
-    return new Queue(this._elements.slice(this._offset));
-  }
+    /**
+     * Checks if the queue is empty.
+     * @public
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this.size() === 0;
+    }
 
-  /**
-   * Creates a queue from an existing array.
-   * @public
-   * @static
-   * @param {array} elements
-   * @return {Queue}
-   */
-  static fromArray(elements) {
-    return new Queue(elements);
-  }
+    /**
+     * Returns the remaining elements in the queue as an array.
+     * @public
+     * @returns {array}
+     */
+    toArray() {
+        return this._elements.slice(this._offset);
+    }
+
+    interleave() {
+        if (this._elements.length % 2 != 0) return false;
+        let temp = [];
+        let halfSize = this._elements.length / 2;
+        for (let i = 0; i < halfSize; i++) {
+            temp.push(this._elements[0]);
+            this._elements.shift();
+        }
+        while (temp.length != 0) {
+            this._elements.push(temp[0]);
+            this._elements.push(this._elements[0]);
+            this._elements.shift();
+            temp.shift();
+        }
+        return true;
+    }
+
+    /**
+     * Creates a shallow copy of the queue.
+     * @public
+     * @return {Queue}
+     */
+    clone() {
+        return new Queue(this._elements.slice(this._offset));
+    }
+
+    clear() {
+        this._elements = [];
+        this._offset = 0;
+    }
+
+    /**
+     * Creates a queue from an existing array.
+     * @public
+     * @static
+     * @param {array} elements
+     * @return {Queue}
+     */
+    static fromArray(elements) {
+        return new Queue(elements);
+    }
 }
 
 exports.Queue = Queue;
--- test/queue.test.js
+++ test/queue.test.js
@@ -2,92 +2,191 @@ const { expect } = require('chai');
 const { Queue } = require('../src/queue');
 
 describe('Queue unit tests', () => {
-  let queue;
+    let queue;
 
-  describe('new Queue()', () => {
-    it('creates an empty queue', () => {
-      queue = new Queue();
+    describe('new Queue()', () => {
+        it('creates an empty queue', () => {
+            queue = new Queue();
+        });
     });
-  });
 
-  describe('Queue.fromArray(list)', () => {
-    it('creates a queue from an existing array', () => {
-      const q = Queue.fromArray([1, 2, 3]);
-      expect(q.front()).to.equal(1);
-      expect(q.size()).to.equal(3);
+    describe('Queue.fromArray(list)', () => {
+        it('creates a queue from an existing array', () => {
+            const q = Queue.fromArray([1, 2, 3]);
+            expect(q.front()).to.equal(1);
+            expect(q.size()).to.equal(3);
+        });
     });
-  });
 
-  describe('.enqueue(element)', () => {
-    it('should enqueue 3 elements to the stack', () => {
-      queue.enqueue(1);
-      queue.enqueue(8);
-      queue.push(45);
+    describe('.enqueue(element)', () => {
+        it('should enqueue 3 elements to the stack', () => {
+            queue.enqueue(1);
+            queue.enqueue(8);
+            queue.push(45);
+        });
     });
-  });
 
-  describe('.size()', () => {
-    it('should have size of 3', () => {
-      expect(queue.size()).to.equal(3);
+    describe('.reverse()', () => {
+        it('reverse the queue', () => {
+            let rq = new Queue();
+            rq.enqueue(1);
+            rq.enqueue(2);
+            rq.enqueue(3);
+            rq.enqueue(4);
+
+            expect(rq.toArray()).to.deep.equal([1, 2, 3, 4]);
+
+            rq.reverse();
+
+            expect(rq.toArray()).to.deep.equal([4, 3, 2, 1]);
+        });
     });
-  });
 
-  describe('.front()', () => {
-    it('should peek the front element', () => {
-      expect(queue.front()).to.equal(1);
+    describe('.size()', () => {
+        it('should have size of 3', () => {
+            expect(queue.size()).to.equal(3);
+        });
     });
-  });
 
-  describe('.back()', () => {
-    it('should peek the back element', () => {
-      expect(queue.back()).to.equal(45);
+    describe('.front()', () => {
+        it('should peek the front element', () => {
+            expect(queue.front()).to.equal(1);
+        });
     });
-  });
 
-  describe('.isEmpty()', () => {
-    it('should not be empty', () => {
-      expect(queue.isEmpty()).to.equal(false);
+    describe('.sort()', () => {
+        it('sort the queue', () => {
+            const elements = [5, 4, 3, 2, 1];
+            const sq = Queue.fromArray(elements);
+
+            expect(sq.toArray()).to.deep.equal(elements);
+
+            sq.sort();
+
+            expect(sq.toArray()).to.deep.equal([1, 2, 3, 4, 5]);
+        });
     });
-  });
 
-  describe('.clone()', () => {
-    it('clone a queue', () => {
-      queue.dequeue();
-
-      const clone = queue.clone();
-      clone.dequeue();
-
-      expect(clone.front()).to.equal(45);
-      expect(clone.size()).to.equal(1);
-      expect(queue.front(8));
-      expect(queue.size()).to.equal(2);
+    describe('.back()', () => {
+        it('should peek the back element', () => {
+            expect(queue.back()).to.equal(45);
+        });
     });
-  });
 
-  describe('toArray()', () => {
-    it('should convert the queue into an array', () => {
-      expect(queue.toArray()).to.deep.equal([8, 45]);
-    });
-  });
+    describe('.reverseFirstK()', () => {
+        it('should return false for empty array', () => {
+            const rq = new Queue();
+            const k = 5;
+            const res = rq.reverseFirstK(k);
+            expect(res).to.equal(false);
+        });
 
-  describe('dequeue()', () => {
-    it('should dequeue all elements', () => {
-      expect(queue.dequeue()).to.be.equal(8);
-      expect(queue.pop()).to.be.equal(45);
-    });
-  });
+        it('should return false for k > number of elements', () => {
+            const rq = new Queue();
+            rq.push(1);
+            rq.push(2);
+            rq.push(3);
 
-  describe('.clear()', () => {
-    it('should clear the queue', () => {
-      queue.enqueue(1);
-      queue.enqueue(2);
-      queue.enqueue(3);
-      queue.clear();
-      expect(queue.dequeue()).to.be.equal(null);
-      expect(queue.front()).to.be.equal(null);
-      expect(queue.back()).to.be.equal(null);
-      expect(queue.size()).to.be.equal(0);
-      expect(queue.isEmpty()).to.be.equal(true);
+            const k = 5;
+
+            const res = rq.reverseFirstK(k);
+            expect(res).to.equal(false);
+        });
+
+        it('reverse in groups of k and return true', () => {
+            const rq = new Queue();
+            rq.push(10);
+            rq.push(20);
+            rq.push(30);
+            rq.push(40);
+            rq.push(50);
+            rq.push(60);
+            rq.push(70);
+            rq.push(80);
+            rq.push(90);
+            rq.push(100);
+
+            const k = 5;
+
+            expect(rq.toArray()).to.deep.equal([
+                10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+            ]);
+
+            const res = rq.reverseFirstK(k);
+            expect(res).to.equal(true);
+
+            expect(rq.toArray()).to.deep.equal([
+                50, 40, 30, 20, 10, 60, 70, 80, 90, 100
+            ]);
+        });
     });
-  });
+
+    describe('.isEmpty()', () => {
+        it('should not be empty', () => {
+            expect(queue.isEmpty()).to.equal(false);
+        });
+    });
+
+    describe('.clone()', () => {
+        it('clone a queue', () => {
+            queue.dequeue();
+
+            const clone = queue.clone();
+            clone.dequeue();
+
+            expect(clone.front()).to.equal(45);
+            expect(clone.size()).to.equal(1);
+            expect(queue.front(8));
+            expect(queue.size()).to.equal(2);
+        });
+    });
+
+    describe('.interleave()', () => {
+        const wrongElements = [1, 2, 3, 4, 5];
+        const goodElements = [1, 2, 3, 4];
+        const expectedResult = [1, 3, 2, 4];
+
+        it('should abort operations on an odd length queue', () => {
+            const q = Queue.fromArray(wrongElements);
+            const res = q.interleave();
+            expect(res).to.equal(false);
+        });
+
+        it('should interleave elements of odd and even positions', () => {
+            const q = Queue.fromArray(goodElements);
+            expect(q.toArray()).to.deep.equal(goodElements);
+
+            const res = q.interleave();
+            expect(res).to.equal(true);
+
+            expect(q.toArray()).to.deep.equal(expectedResult);
+        });
+    });
+
+    describe('toArray()', () => {
+        it('should convert the queue into an array', () => {
+            expect(queue.toArray()).to.deep.equal([8, 45]);
+        });
+    });
+
+    describe('dequeue()', () => {
+        it('should dequeue all elements', () => {
+            expect(queue.dequeue()).to.be.equal(8);
+            expect(queue.pop()).to.be.equal(45);
+        });
+    });
+
+    describe('.clear()', () => {
+        it('should clear the queue', () => {
+            queue.enqueue(1);
+            queue.enqueue(2);
+            queue.enqueue(3);
+            queue.clear();
+            expect(queue.dequeue()).to.be.equal(null);
+            expect(queue.front()).to.be.equal(null);
+            expect(queue.back()).to.be.equal(null);
+            expect(queue.size()).to.be.equal(0);
+            expect(queue.isEmpty()).to.be.equal(true);
+        });
+    });
 });
